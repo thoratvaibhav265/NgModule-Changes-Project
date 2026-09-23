228f870fda8113b162e84829363b9d311a37726c
Vaibhav <thoratvaibhav265@gmail.com>
2026-09-23 20:15:24 +0530

    Changes in Angular Module done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [BrowserModule,FormsModule]
 })  
 export class AppModule {}
